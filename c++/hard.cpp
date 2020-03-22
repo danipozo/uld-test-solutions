2f9fbeff2486704f835e0628632d6f70733f2dbc
Dani Pozo <danipozo@correo.ugr.es>
2020-03-22 02:35:49 +0100

    Add C++ versions of medium and hard tests

---
+++ c++/hard.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
+#include "Core"
+#include "cartesian_geom/cartesian_kernel.h"
+
+typedef point<Cartesian<double>> Point;
+
+/* Difference between two points.
+ * This is a quick fix needed because not all variants
+ * of the operator- that are needed are implemented in point.
+ */
+Point diff(Point x, Point y) {
+  Point ret(x.dimension());
+  for(size_t i = 0; i < x.dimension(); i++) {
+    ret.set_coord(i, x[i] - y[i]);
+  }
+
+  return ret;
+}
+
+template<class F>
+Point barzilai_borwein(F grad_f, Point initial_point, double initial_step = 0.01, size_t max_steps = 100) {
+  Point gradient_last = grad_f(initial_point);
+  Point point_last = initial_point;
+  Point point = diff(initial_point, initial_step*gradient_last);
+  Point gradient = grad_f(point);
+
+  Point s_last = point - point_last;
+  Point y_last = gradient - gradient_last;
+
+  Point zero(gradient.dimension());
+  for(size_t i = 0; i < max_steps; i++) {
+    if(gradient == zero) {
+      break;
+    }
+
+    double bb_step = s_last.dot(s_last) / y_last.dot(s_last);
+
+    point_last = point;
+    gradient_last = gradient;
+
+    point = diff(point, bb_step*gradient);
+    gradient = grad_f(point);
+
+    s_last = point - point_last;
+    y_last = gradient - gradient_last;
+  }
+
+  return point;
+}
+
+Point oracle(const Point& x) {
+  Point ret(2);
+
+  ret.set_coord(0, 2*(x[0]*exp(x[1]) - 2*x[1]*exp(-x[0]))*(exp(x[1]) + 2*x[1]*exp(x[0])));
+  ret.set_coord(1, 2*(x[0]*exp(x[1]) -2*x[1]*exp(-x[0]))*(x[0]*x[1]*exp(x[1]) - 2*exp(-x[0])));
+
+  return ret;
+}
+
+double f(const Point& x) {
+  return std::pow(x[0]*exp(x[1]) - 2*x[1]*exp(-x[0]), 2);
+}
+
+int main() {
+  Point initial(2);
+  initial.set_coord(0, 3.1416);
+  initial.set_coord(1, 3.1416);
+  Point minimum = barzilai_borwein(oracle, initial);
+
+  std::cout << "Minimum: " << minimum[0] << ", " << minimum[1] << std::endl;
+  std::cout << "Value:   " << f(minimum) << std::endl;
+}
---
+++ c++/medium.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
+
+#include "Core"
+#include "cartesian_geom/cartesian_kernel.h"
+#include <chrono>
+#include <boost/random.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/uniform_real_distribution.hpp>
+#include "hpolytope.h"
+#include "samplers.h"
+
+typedef point<Cartesian<double>> Point;
+
+/* Ball walk with the uniform target distribution.
+ *
+ */
+template<class Polytope>
+Point ball_walk_uniform(const Polytope& poly, double delta, const Point& start, size_t steps = 100) {
+  Point p = start;
+  for(size_t i = 0; i < steps; i++) {
+    ball_walk<std::mt19937>(p, poly, delta);
+  }
+
+  return p;
+}
+
+
+/* Ball walk for log-concave target distribution given by f.
+ * 
+ */
+template<class F, class Polytope>
+Point ball_walk(F f, const Polytope& poly, double delta, const Point& start, size_t steps = 100) {
+  // Random number generation
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> dis(0, 1);
+  
+  Point x = start;
+  for(size_t i = 0; i < steps; i++) {
+    Point y = ball_walk_uniform(poly, delta, x);
+
+    double prob = std::min(1.0, std::exp(f(x) - f(y)));
+    if(dis(gen) < prob) {
+      x = y;
+    }
+  }
+
+  return x;
+}
+
+/* Strongly convex oracle for a gaussian
+ * with mean 0 and variance 0.1*I.
+ */
+double variance = 0.1;
+double oracle(const Point& x) {
+  return (1/2.0)*(x[0]*x[0] + x[1]*x[1])/variance;
+}
+
+
+
+size_t n_samples = 1000;
+int main() {
+  // 2-dimensional unit cube
+  HPolytope<Point> poly(2);
+
+  std::vector<Point> samples(n_samples);
+  for(size_t i = 0; i < n_samples; i++) {
+    samples[i] = ball_walk(oracle, poly, 0.01, Point(2), 200);
+  }
+
+  for(auto& s : samples) {
+    std::cout << s[0] << " " << s[1] << std::endl;
+  }
+}
