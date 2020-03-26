e0f42c1c0415e3cfae799e8ede565df47fa54674
Dani Pozo <danipozo@correo.ugr.es>
2020-03-26 18:19:34 +0100

    Fix the R version of the medium test: follow the C++ implementation

--- R/medium.R
+++ R/medium.R
@@ -1,28 +1,41 @@
 library(volesti)
 library(ggplot2)
 
-ball_walk <- function (f, polytope, steps = 100) {
-    points <- sample_points(polytope, steps, 'uniform', walk_step = 200)
+# Sample a point uniformly distributed on
+# the unit (d-1)-dimensional sphere.
+get_direction <- function (d) {
+    p <- rnorm(d)
+    p / norm(p, type="2")
+}
 
+# Sample a point uniformly distributed on
+# the unit d-dimensional ball.
+uniform_ball <- function(d, delta) {
+    r <- delta*runif(1)
+    r*get_direction(d)
+}
+
+# Ball walk for a log-concave distribution given a strongly
+# convex function. polytope needs to be an Hpolytope.
+ball_walk <- function (f, polytope, start, delta = 0.01, steps = 100) {
     # Initial point
-    x <- points[,1]
+    x <- start
 
-    # Samples used for filter
+    # Uniform samples from [0,1] used to jump to next point
+    # with a certain probability.
     epsilon <- runif(steps)
     
     for (i in 2:steps) {
-        y <- points[,i]
+        # Uniformly sample a point from the ball
+        # of radius delta around x.
+        y <- x + uniform_ball(length(x), delta)
 
         # Quotient of densities of induced
-        # log-concave distribution
+        # log-concave distribution.
         p <- exp(f(x) - f(y))
-        
-        if (p >= 1) {
-            x <- y
-            next
-        }
 
-        if (epsilon[i] < p) {
+        y_in_poly <- polytope$A %*% y <= polytope$b
+        if (epsilon[i] < p && y_in_poly) {
             x <- y
         }
     }
@@ -38,13 +51,13 @@ oracle <- function(x) {
 
 polytope <- GenCube(2, 'H')
 
-n_samples = 10000
+n_samples = 1000
 
-# I'm sure there's a more elegant way to do this
 samples <- array(1:2*n_samples, c(n_samples,2))
 
+# Sample points using ball walk and plot them
 for (i in 1:n_samples) {
-    samples[i,] <- ball_walk(oracle, polytope, steps = 200)
+    samples[i,] <- ball_walk(oracle, polytope, c(0, 0), steps = 200)
 }
 
 samples <- data.frame(samples)
